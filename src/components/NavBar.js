72e27de3156bb982c33e5a0ba20a79354eeb9c9f
Felipe7575 <fcampagnoli102@alumnos.frh.utn.edu.ar>
2022-09-21 00:15:39 -0300

    Cambie el NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import{useContext, useEffect} from 'react';
 import {AppContext} from '../app/Provider';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import CarritoIcon from './CarritoIcon';
 
@@ -24,47 +26,55 @@ const NavBar = ({CANT}) => {
      setCant(cart.length);
   }, [cart]);
 
+  const navigate = useNavigate();
+  const click= (dir) => {navigate(dir);}
+
   return (
-    <Navbar className="navbarDiv" bg="light" expand="lg">
-      <Container fluid>
-        <Link to="/" className='navBarBrand'> <img src={LOGO} style={{height: 50, width: 50}} alt="LOGO DE 3D RAMOS" ></img> TODO 3D RAMOS  </Link>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" >
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Link className="navbarLink" to="/">INICIO</Link>
-            <NavDropdown className="navbarDropDown"  title="PRODUCTOS" id="navbarScrollingDropdown">
-            
-              <Link className="navbarDropDown-item" to="/productos/1">Filamento PLA</Link> 
-              <Link className="navbarDropDown-item" to="/productos/2">Filamento ABS</Link> 
-              <Link className="navbarDropDown-item" to="/productos/3">Impresoras 3D</Link>  
-
-               <NavDropdown.Divider />
-              <Link className="navbarDropDown-item" to="/productos/0">CATALOGO</Link> 
-
-            </NavDropdown> 
-
-            <Link className="navbarLink" to="/contact">CONTACTO</Link>   
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Buscar..."
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">BUSCAR</Button>
-          </Form>
-          <CarritoIcon CANT={cant}></CarritoIcon>
-        </Navbar.Collapse>
+    <Navbar key='md' bg="light" expand="md" className="mb-3">
+      <Container fluid className='containerNavBar'>
+        <Navbar.Brand onClick={()=>click("/")} className="navBarBrand">
+            <img
+              alt=""
+              src={LOGO}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+            />{' '}
+            <p>TODO 3D RAMOS</p>
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-md`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+              TODO 3D RAMOS
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link onClick={()=>click("/")}>INICIO</Nav.Link>
+              <Nav.Link onClick={()=>click("/contact")}>CONTACTOS</Nav.Link>
+              <NavDropdown
+                title="PRODUCTOS"
+                id={`offcanvasNavbarDropdown-expand-md`}
+              >
+                <NavDropdown.Item onClick={()=>click("/productos/1")}>Filamento PLA</NavDropdown.Item>
+                <NavDropdown.Item onClick={()=>click("/productos/2")}>Filamento ABS</NavDropdown.Item>
+                <NavDropdown.Item onClick={()=>click("/productos/3")}>IMPRESORAS</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={()=>click("/productos/0")}>CATALOGO</NavDropdown.Item>
+              </NavDropdown>
+              <div carritoIcon> 
+                <CarritoIcon CANT={cant}></CarritoIcon>
+              </div>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
-    </Navbar>
-  
-  
-   
+    </Navbar>  
   )
 }
 
